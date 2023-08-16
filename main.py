8160abdf85c5ddb12b0b1d8fb22a36f989937cc0
isadione <isadi@LAPTOP-A6R0FKV3>
2023-08-15 22:42:39 -0300

    archivos

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.ships import Cargo
 from src.ships import Cruise
 
 
-def esNumero(n): 
+def numerito(n): 
 	try:
 		float(n)
 		return True
@@ -18,32 +18,30 @@ def main() -> None:
 	lista_de_barcos = [] 	
 
 	# la r es de read, solo lectura
-	with open('ships.csv','r') as archivoBarcos:
-		lectorBarcos = csv.reader(archivoBarcos)
+	with open('ships.csv','r') as fileCSV_: # fileCSV_ es el archivo abierto para leer
+		fp = csv.reader(fileCSV_) # fp es el puntero dentro del archivo solo para la lectura
 
-		
-		next(lectorBarcos) # next() hace avanzar una linea (fila) al lector de archivos, - se saltea los headers -
+		next(fp) # next() hace avanzar una linea (fila) al lector de archivos, - se saltea los headers -
 		cant = 0
 
-	
-		for row in lectorBarcos: # row es la columna de la fila en la que esté ubicado el lector
+		for row in fp: # row es la columna de la fila en la que esté ubicado el lector
 			if row[2] == "" and row[3] == "" :
-				if esNumero(row[0]) and esNumero(row[1]):
+				if numerito(row[0]) and numerito(row[1]): # devuelve true si es un numero y false si no lo es
 					barco1 = Ship(row[0], row[1])
 					lista_de_barcos.append(barco1) # con append agrego un elemento a la lista
 			
 			elif row[2] != "" and row[3] == "" :
-				if esNumero(row[0]) and esNumero(row[1]) and esNumero(row[2]):
-					barco2 = Cruise(row[2], row[0], row[1]) # con esa condicion chequeo que el barco sea un crucero
-					lista_de_barcos.append(barco2)
+				if numerito(row[0]) and numerito(row[1]) and numerito(row[2]):
+					crucero = Cruise(row[2], row[0], row[1]) # con esa condicion chequeo que el barco sea un crucero
+					lista_de_barcos.append(crucero)
 
 			elif row[2] != "" and row[3] != "" :
-				if esNumero(row[0]) and esNumero(row[1]) and esNumero(row[2]) and esNumero(row[3]):
-					barco3 = Cargo(row[2], row[3], row[0], row[1]) # barco de carga 
-					lista_de_barcos.append(barco3)
+				if numerito(row[0]) and numerito(row[1]) and numerito(row[2]) and numerito(row[3]):
+					buquedecarga = Cargo(row[2], row[3], row[0], row[1]) # barco de carga 
+					lista_de_barcos.append(buquedecarga)
 
 			else:
-				cant += 1
+				print("Barco mal registrado")
 
 if __name__ == "__main__":
   main()
